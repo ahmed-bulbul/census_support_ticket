282aa31680aa0939af12720ad579ca83c54ba09d
Bulbul Ahmed <70557643+bulbul-deploy@users.noreply.github.com>
2022-04-25 14:26:26 +0600

    add

--- Backend/src/main/java/com/census/support/util/SysMessage.java
+++ Backend/src/main/java/com/census/support/util/SysMessage.java
@@ -7,6 +7,8 @@ public class SysMessage {
     public static final String CLOSE_MSG = "Dear user your ticket has been resolved. Your ticket number is:  ";
     public static final String ASSIGN_MSG = "Dear user your ticket has been assigned to ";
     public static final String SOLVED_MSG = "Dear user your ticket has been solved. Your ticket number is:  ";
+    public static final String TERMINATE_MSG = "Dear user your ticket has been terminated. Please contact your" +
+            " nearest walton support center. Your ticket number is:  ";
 
 
     public static final String OPEN_STS = "OPEN";
@@ -17,6 +19,7 @@ public class SysMessage {
     public static final String RECEIVED_T2_STS = "RECEIVED_T2";
     public static final String HOLD_T2_STS = "HOLD_T2";
     public static final String RESOLVED_T2_STS = "SOLVED_T2"; //solved
+    public static final String TERMINATE_STS = "TERMINATE";
 
 
 }
