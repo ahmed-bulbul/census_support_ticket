b60f98afdd0ad33084b143236d9a6221449aa9ff
Bulbul Ahmed <70557643+bulbul-deploy@users.noreply.github.com>
2022-05-07 12:42:41 +0600

    ss

--- Backend/src/test/java/com/census/support/acl/auth/AuthControllerTest.java
+++ Backend/src/test/java/com/census/support/acl/auth/AuthControllerTest.java
@@ -1,4 +1,25 @@
+package com.census.support.acl.auth;
+
+import com.census.support.acl.security.jwt.config.JwtUtils;
+import com.census.support.acl.user.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import static org.junit.jupiter.api.Assertions.*;
-class AuthControllerTest {
-  
+
+public class AuthControllerTest {
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    UserService userService;
+
+
+    @Test
+    @DisplayName("Login Controller Test")
+    void userLogin() {
+        assertTrue(true);
+    }
 }
